895a98dd1da314b9da86c1c09464628c00bf2a7c
Brendan Moran <brendan.moran@arm.com>
2016-01-27 16:52:54 +0000

    Add some preliminary documentation Remove several members that aren’t used

--- mbed-drivers/I2C.h
+++ mbed-drivers/I2C.h
@@ -30,49 +30,110 @@
 #include "Transaction.h"
 #endif
 
-namespace mbed {
+/**
+ * \file
+ * \brief A generic interface for I2C peripherals
+ *
+ * The I2C class interfaces with an I2C Resource manager in order to initiate Transactions
+ * and receive events. The I2CTransaction class encapsulates all I2C transaction parameters.
+ * The I2CResourceManager class is a generic interface for implementing I2C resource managers.
+ * This will allow for additional classes of I2C device, for example, a bitbanged I2C master.
+ *
+ * I2C Resource managers are instantiated statically and initialized during global init.
+ * There is one Resource Manager per logical port. Logical ports could consist of:
+ *
+ * * Onchip I2C controllers
+ * * I2C Bridges (SPI->I2C bridge, I2C->I2C bridge, etc.)
+ * * Bit banged I2C
+ * * Bit banged I2C over SPI GPIO expander
+ * * More...
+ *
+ * Currently only onchip I2C controllers are supported.
+ */
 
+namespace mbed {
 namespace detail {
-class I2CResourceManager;
+/**
+ * A Transaction container for I2C
+ */
 class I2CTransaction {
 public:
     I2CTransaction():
         next(nullptr), address(0), callback(nullptr), event(0), hz(100000), repeated(false)
     {}
+    /**
+     * Construct an I2C transaction and set the destination address at the same time
+     *
+     * @param[in] address set the I2C destination address
+     */
     I2CTransaction(uint16_t address):
         next(nullptr), address(address), callback(nullptr), event(0), hz(100000), repeated(false)
     {}
+    /**
+     * Copy an I2C transaction
+     * Does not copy the next pointer or the callback pointer
+     */
     I2CTransaction(const I2CTransaction & t):
         next(nullptr), address(t.address),
         tx(t.tx), rx(t.rx),
         callback(t.callback), event(t.event), hz(t.hz), repeated(t.repeated)
     {}
+    /* The next pointer is used to chain Transactions together into a queue.
+     * TODO: Should this be added as a wrapper class in the cpp file, since it's not relevant to the user-facing API?
+     */
     volatile I2CTransaction * next;
-    int address;
-    Buffer tx;
-    Buffer rx;
-    event_callback_t callback;
-    int event;
-    unsigned hz;
-    bool repeated;
+    uint16_t address;          ///< The target I2C address to communicate with
+    Buffer tx;                 ///< The buffer (```void *```/size pair) to send from
+    Buffer rx;                 ///< The buffer (```void *```/size pair) to receive into
+    event_callback_t callback; ///< The function pointer to call when the transaction terminates
+    int event;                 ///< The event mask for the callback
+    unsigned hz;               ///< The I2C frequency to use for the transaction
+    bool repeated;             ///< If repeated is true, do not generate a stop condition
 };
 
-extern I2CResourceManager * I2COwners[MODULES_SIZE_I2C];
-
+/**
+ * The base resource manager class for I2C
+ */
 class I2CResourceManager {
 public:
+    /* Copying and moving Resource Managers would have unpredictable results */
     I2CResourceManager(const I2CResourceManager&) = delete;
     I2CResourceManager(I2CResourceManager&&) = delete;
     operator =(const I2CResourceManager&) = delete;
     operator =(I2CResourceManager&&) = delete;
+    /* Add a transaction to the transaction queue of the associated logical I2C port
+     * This is the single user-facing API of the I2C Resource manager.
+     *
+     * If the peripheral is idle, calls power_up()
+     * @param[in] transaction Queue this transaction
+     * @return the result of validating the transaction
+     */
     int post_transaction(I2CTransaction &transaction);
 protected:
+    /** These APIs are the interfaces that must be supplied by a derived Resource Manager */
+    /** Starts the transaction at the head of the queue */
     virtual int start_transaction() = 0;
+    /** Validates the transaction according to the criteria of the derived Resource Manager */
     virtual int validate_transaction(I2CTransaction &transaction) = 0;
+    /** Powers down the associated I2C controller */
+    virtual int power_down() = 0;
+    /** Power up the associated I2C controller */
+    virtual int power_up() = 0;
+protected:
+    /** Handle an event
+     * Starts the next transfer
+     * If there are no more transfers queued, calls power_down()
+     * Then, calls the event associated with the completed transfer.
+     * Finally, frees the associated event.
+     *
+     * @param[in] event the source of the current handler call
+     */
     void process_event(int event);
     I2CResourceManager();
     ~I2CResourceManager();
+    // A shared pool of transaction objects for all I2C resource managers
     static PoolAllocator TransactionPool;
+    // The head of the transaction queue
     volatile I2CTransaction * TransactionQueue;
 };
 } // namespace detail
@@ -141,6 +202,7 @@ public:
         TransferAdder & tx(uint8_t *buf, size_t len);
         TransferAdder & rx(Buffer buf);
         TransferAdder & tx(Buffer buf);
+        TransferAdder & frequency(uint32_t hz);
         TransferAdder & callback(const event_callback_t& callback, int event = I2C_EVENT_TRANSFER_COMPLETE);
         TransferAdder & repreated_start();
         int apply();
@@ -155,21 +217,7 @@ public:
     TransferAdder transfer_to(int address);
 
 protected:
-    typedef TwoWayTransaction<event_callback_t> transaction_data_t;
-    typedef Transaction<I2C, transaction_data_t> transaction_t;
-
-    void irq_handler_asynch(void);
-    transaction_data_t _current_transaction;
-    CThunk<I2C> _irq;
-    DMAUsage _usage;
-#endif
-
-protected:
-    void aquire();
-
-    i2c_t _i2c;
-    static I2C  *_owner;
-    int         _hz;
+    int _hz;
 };
 
 } // namespace mbed
